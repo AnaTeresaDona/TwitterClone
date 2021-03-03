8d73a0903e7efe9f23c761296af46fac1ea62511
AnaTeresaDona <anatdona@gmail.com>
2021-03-02 23:38:46 -0300

    Proyect Twitter, branch hito2 first commit. Creation of followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :likes
   has_many :liked_tweets, through: :likes, source: :tweet #es un método que permite saber a qué tweet el usuario le hizo like.
+  has_many :friends, dependent: :destroy
+
+
+  def users_followed
+    arr_ids = self.friends.pluck(:friend_id) #ahora en ese arreglo (arr_ids) tengo guardados todos mis amigos.
+    User.find(arr_ids)
+  end
+
+  def is_following?(user)
+    users_followed.include?(user)
+  end
 
   def to_s
     username
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :tweets do
+  resources :tweets do  
     post 'likes', to: 'tweets/likes'
     post 'retweet', to: 'tweets/retweet'
   end
   devise_for :users
   get 'home/index'
 
+  post 'follow/:user_id', to: 'users#follow', as: 'users_follow'
 
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
